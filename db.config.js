2d115b47634920a9e7b17976a796b9f932a128ff
lChap701 <chapmanlucas201@gmail.com>
2021-10-31 12:10:13 -0500

    Added module to setup a DB connection

---
+++ db.config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+/**
+ * Attempts to connect to the DB and displays the result in the console
+ */
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connection Established");
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
