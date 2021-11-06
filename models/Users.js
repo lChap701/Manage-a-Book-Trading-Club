8bf0dd9319b6b09e4075d45633ad350618d5c7af
lChap701 <chapmanlucas201@gmail.com>
2021-11-06 18:41:34 -0500

    Improved validation

--- models/Users.js
+++ models/Users.js
@@ -2,36 +2,53 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // Note: Password is only required when OAuth is not used to create an account!!
-const userSchema = new Schema({
-  username: { type: String, trim: true, required: "{PATH} is required" },
-  password: {
-    type: String,
-    trim: true,
-    required: [() => !this.oauth, "{PATH} is required"],
-  },
-  name: { type: String, trim: true },
-  address: { type: String, trim: true },
-  city: { type: String, trim: true },
-  state: { type: String, trim: true },
-  country: { type: String, trim: true },
-  zipPostal: { type: String, trim: true },
-  oauth: { type: Boolean, default: false },
-  accounts: [
-    {
-      id: { type: String, unique: true },
-      username: String,
-      name: String,
-      url: { type: String, unique: true },
-      photos: [{ value: String }],
-      provider: {
-        type: String,
-        enum: ["github", "facebook", "twitter", "google", "microsoft"],
-      },
+const userSchema = new Schema(
+  {
+    username: { type: String, trim: true, required: "{PATH} is required" },
+    password: {
+      type: String,
+      trim: true,
+      required: [() => !this.oauth, "{PATH} is required"],
     },
-  ],
-  books: [{ type: Schema.Types.ObjectId, ref: "Books" }],
-  requests: [{ type: Schema.Types.ObjectId, ref: "Requests" }],
-});
+    name: { type: String, trim: true },
+    address: { type: String, trim: true },
+    city: { type: String, trim: true },
+    state: { type: String, trim: true },
+    country: { type: String, trim: true },
+    zipPostal: { type: String, trim: true },
+    oauth: { type: Boolean, default: false },
+    accounts: [
+      {
+        id: {
+          type: String,
+          required: [() => this.oauth, "{PATH} is required"],
+        },
+        username: {
+          type: String,
+          required: [() => this.oauth, "{PATH} is required"],
+        },
+        name: String,
+        url: {
+          type: String,
+          required: [() => this.oauth, "{PATH} is required"],
+        },
+        photos: [{ value: String }],
+        provider: {
+          type: String,
+          enum: ["github", "facebook", "twitter", "google", "microsoft"],
+          required: [() => this.oauth, "{PATH} is required"],
+        },
+      },
+    ],
+    books: [{ type: Schema.Types.ObjectId, ref: "Books" }],
+    requests: [{ type: Schema.Types.ObjectId, ref: "Requests" }],
+  },
+  {
+    timestamps: {
+      updatedAt: false,
+    },
+  }
+);
 
 /**
  * Model for the 'users' collection
