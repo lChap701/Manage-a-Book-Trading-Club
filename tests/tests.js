79c3974ec4b368d0473a2855d6f4e8075030d1d7
lChap701 <chapmanlucas201@gmail.com>
2021-11-08 14:29:51 -0600

    Added test suite for /api/users

--- tests/tests.js
+++ tests/tests.js
@@ -33,6 +33,7 @@ suite("Unit Tests", () => {
           address: "123 6th Street",
           city: "Big City",
           state: "Iowa",
+          country: "United States",
           zipPostal: "52061",
         };
 
@@ -181,6 +182,90 @@ suite("Unit Tests", () => {
             done();
           });
       });
+    });
+  });
+
+  suite("Testing /api/users", () => {
+    test("1) GET Test", (done) => {
+      chai
+        .request(app)
+        .get("/api/users")
+        .end((err, res) => {
+          assert.equal(res.status, 200, "response status should be 200");
+          assert.isArray(
+            JSON.parse(res.text),
+            "response should return an array"
+          );
+          assert.equal(
+            JSON.parse(res.text).length,
+            2,
+            "response should return two objects"
+          );
+          assert.property(
+            JSON.parse(res.text)[0],
+            "_id",
+            "response should return objects with a property of '_id'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[0],
+            "username",
+            "dummyUser1",
+            "response should return an array containing an object with a property of 'name' that equals 'dummyUser1'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[1],
+            "username",
+            "dummyUser2",
+            "response should return an array containing an object with a property of 'name' that equals 'dummyUser2'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[0],
+            "city",
+            "Big City",
+            "response should return an array containing an object with a property of 'city' that equals 'Big City'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[1],
+            "city",
+            "null",
+            "response should return an array containing an object with a property of 'city' that equals 'null'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[0],
+            "state",
+            "Iowa",
+            "response should return an array containing an object with a property of 'state' that equals 'Iowa'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[1],
+            "state",
+            "null",
+            "response should return an array containing an object with a property of 'state' that equals 'null'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[0],
+            "country",
+            "United States",
+            "response should return an array containing an object with a property of 'country' that equals 'United States'"
+          );
+          assert.propertyVal(
+            JSON.parse(res.text)[1],
+            "country",
+            "null",
+            "response should return an array containing an object with a property of 'country' that equals 'null'"
+          );
+          assert.property(
+            JSON.parse(res.text)[0],
+            "books",
+            "response should return objects with a property of 'books'"
+          );
+          assert.property(
+            JSON.parse(res.text)[0],
+            "incomingRequests",
+            "response should return objects with a property of 'incomingRequests'"
+          );
+          done();
+        });
     });
   });
 });
