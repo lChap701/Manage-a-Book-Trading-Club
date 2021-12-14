75431dbb60f9bc51c5f06bb87367027c969b73f1
Lucas Chapman <chapmanlucas201@gmail.com>
2021-12-13 18:44:13 -0600

    Implemented new model and removed updateRequest()

--- crud.js
+++ crud.js
@@ -1,6 +1,7 @@
 const Users = require("./models/Users");
 const Books = require("./models/Books");
 const Requests = require("./models/Requests");
+const Trades = require("./models/Trades");
 
 /**
  * Module for running CRUD operations on the DB
@@ -11,19 +12,20 @@ const crud = {
   addUser: (data) => new Users(data).save(),
   addBook: (data) => new Books(data).save(),
   addRequest: (data) => new Requests(data).save(),
+  addTrade: (data) => new Trades(data).save(),
   getUsers: () => Users.find(),
   getAllBooks: () => Books.find(),
   getBooks: (user) => Books.find({ user: user }),
   getRequests: () => Requests.find(),
+  getTrades: () => Trades.find(),
   getUser: (data) => Users.findOne(data),
   getBook: (id) => Books.findOne({ _id: id }),
   getRequest: (id) => Requests.findOne({ _id: id }),
   updateUser: (id, data) => Users.updateOne({ _id: id }, data),
-  updateRequest: (id) =>
-    Requests.updateOne({ _id: id }, { traded: true, tradedAt: new Date() }),
   deleteUser: (id) => Users.deleteOne({ _id: id }),
   deleteBook: (id) => Books.deleteOne({ _id: id }),
   deleteRequest: (id) => Requests.deleteOne({ _id: id }),
+  deleteTrades: (request) => Trades.deleteMany({ request: request }),
 };
 
 module.exports = crud;
