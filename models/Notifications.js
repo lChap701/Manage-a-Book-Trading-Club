d0a80d3804bd4c05bcd1a001fa278e8eddd12d5e
lChap701 <chapmanlucas201@gmail.com>
2022-01-21 15:21:31 -0600

    Added model for the 'notifications' collection

---
+++ models/Notifications.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const notificationSchema = new Schema(
+  {
+    message: { type: String, required: "{PATH} is required" },
+    user: { type: Schema.Types.ObjectId, ref: "Users" },
+  },
+  {
+    timestamps: {
+      createdAt: "sentOn",
+      updatedAt: false,
+    },
+  }
+);
+
+/**
+ * Model for the 'notifications' collection
+ * @module ./models/Notifications
+ *
+ */
+const Notifications = mongoose.model("Notifications", notificationSchema);
+
+module.exports = Notifications;
